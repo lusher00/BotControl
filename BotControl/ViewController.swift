888c849c6cbc79b68c538ddc2c6d01397085ceb1
lusher00 <rlush@escortmac12.local>
2016-10-13 14:29:36 -0400

    Initial Commit

---
+++ BotControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BotControl
+//
+//  Created by Ryan Lush on 10/13/16.
+//  Copyright © 2016 Ryan Lush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
