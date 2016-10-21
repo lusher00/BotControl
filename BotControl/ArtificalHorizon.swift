3f813a2881ed61e0493e26359ddf6af5e22607a1
lusher00 <rlush@escortmac12.local>
2016-10-21 09:49:58 -0400

    sick of summarizing bullshit

--- BotControl/ArtificalHorizon.swift
+++ BotControl/ArtificalHorizon.swift
@@ -10,65 +10,75 @@ import UIKit
 
 class ArtificalHorizon: UIView {
 
-    private let imgHorizon = UIImage.init(named:"horizon")!
-    private let imgBezel = UIImage.init(named:"bezel_transparent")!
-    private let imgWings = UIImage.init(named:"wings_transparent")!
+    private var imgHorizon : UIImage
+    private var imgBezel : UIImage
+    private var imgWings : UIImage
     
     private var imgViewHorizon : UIImageView
     private var imgViewBezel : UIImageView
     private var imgViewWings : UIImageView
     
     private var svHorizon : UIScrollView
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        didLoad()
-    }
+   
     
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        didLoad()
-    }
-    
-    func didLoad() {
-        //Place your initialization code here
         
-        //I actually create & place constraints in here, instead of in
-        //updateConstraints
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
+        imgHorizon = UIImage.init(named:"horizon")!
+        imgBezel = UIImage.init(named:"bezel_transparent")!
+        imgWings = UIImage.init(named:"wings_transparent")!
         
-        //Custom manually positioning layout goes here (auto-layout pass has already run first pass)
-    }
-    
-    override func updateConstraints() {
-        super.updateConstraints()
-        
-        //Disable this if you are adding constraints manually
-        //or you're going to have a 'bad time'
-        //self.translatesAutoresizingMaskIntoConstraints = false
-        
-        //Add custom constraint code here
-    }
-
-    override func draw(_ rect: CGRect) {
         imgViewHorizon = UIImageView.init()
-        imgViewHorizon = UIImageView(frame: CGRect(x: 0, y: 0, width: imgHorizon.size.width, height: imgHorizon.size.height))
-        imgViewHorizon.contentMode = UIViewContentMode.center
+        imgViewBezel = UIImageView.init()
+        imgViewWings = UIImageView.init()
+        
+        svHorizon = UIScrollView.init()
+        
+        super.init(coder: aDecoder)
+        
+        //self.clipsToBounds = true
+        
+        imgViewHorizon = UIImageView(frame: CGRect(x: 0, y: 0, width: imgBezel.size.width, height: imgHorizon.size.height))
+        //imgViewHorizon.contentMode = UIViewContentMode.scaleAspectFill
         imgViewHorizon.clipsToBounds = true
-        imgViewHorizon.image = imgHorizon
+        //imgViewHorizon.image = imgHorizon
+        imgViewHorizon.backgroundColor = UIColor.red
         
-        svHorizon = UIScrollView(frame: CGRect(x: 0, y: 0, width: imgHorizon.size.width, height: imgHorizon.size.width))
-        svHorizon.contentSize = CGSize(width: imgHorizon.size.width, height: imgHorizon.size.height)
-        svHorizon.center = self.center
-        svHorizon.contentOffset = CGPoint(x: (imgHorizon.size.width - svHorizon.frame.width) / 2,
-                                                  y: (imgHorizon.size.height - svHorizon.frame.height) / 2)
+        imgViewBezel = UIImageView(frame: CGRect(x: 0, y: 0, width: imgBezel.size.width, height: imgBezel.size.height))
+        imgViewBezel.contentMode = UIViewContentMode.center
+        imgViewBezel.clipsToBounds = true
+        imgViewBezel.image = imgBezel
         
+        imgViewWings = UIImageView(frame: CGRect(x: 0, y: 0, width: imgBezel.size.width, height: imgBezel.size.height))
+        imgViewWings.contentMode = UIViewContentMode.center
+        imgViewWings.clipsToBounds = true
+        imgViewWings.image = imgWings
+        
+        svHorizon = UIScrollView(frame: CGRect(x: 0, y: 0, width: imgBezel.size.width, height: imgBezel.size.width))
+        svHorizon.contentSize = CGSize(width: imgBezel.size.width, height: imgHorizon.size.height)
+        svHorizon.contentMode = UIViewContentMode.scaleToFill
+        svHorizon.bounces = false
+        svHorizon.backgroundColor = UIColor.cyan
+        svHorizon.contentOffset = CGPoint(x: 0, y: 0)
+        //svHorizon.contentOffset = CGPoint(x: (imgBezel.size.width - svHorizon.frame.width) / 2, y: (imgHorizon.size.height - svHorizon.frame.height) / 2)
+        
+        print(svHorizon.contentOffset)
+        print(svHorizon.contentSize)
+        print(svHorizon.frame.origin)
+        print(imgViewHorizon.frame.size)
+        
+//        addSubview(imgViewHorizon)
         svHorizon.addSubview(imgViewHorizon)
-        self.addSubview(svHorizon)
-
+        addSubview(svHorizon)
+//        addSubview(imgViewBezel)
+//        addSubview(imgViewWings)
     }
+    
+    
+    /*
+    override func intrinsicContentSize() -> CGSize {
+        return CGSize(width: imgBezel.size.width, height: imgHorizon.size.height)
+    }
+    */
+
 
 }
